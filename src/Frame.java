0b1fc71b3ea4370a5a7ff0a70f713df82182f5dd
somename <someemail>
2017-05-11 20:31:21 +0200

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -1,10 +1,12 @@
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
+	private int scoreWithBonus;
 	
 	public Frame(int firstThrow, int secondThrow){
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
+		this.scoreWithBonus = 0;
 	}
 	
 	public int getFirstThrow() {
@@ -19,6 +21,10 @@ public class Frame {
 	public int score(){
 		//to be implemented
 		return firstThrow + secondThrow;
+	}
+	
+	public void addBonus(int bonus){
+		scoreWithBonus = score() + bonus;
 	}
 
 	//returns whether the frame is a strike or not
