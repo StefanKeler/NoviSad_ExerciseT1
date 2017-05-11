ea67336e3b3f3e09edaf489b739358bbf5faf178
somename <someemail>
2017-05-11 20:56:38 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -76,6 +76,35 @@ public class TestBowling {
 		game.addFrame(f2);
 		game.setBonus(f2, 5);		
 	}
+	
+	@Test(expected=BowlingException.class)
+	public void testDodavanjeBonusaNa10Frejm() throws BowlingException{
+		BowlingGame game = new BowlingGame();
+		Frame f1 = new Frame(10,0);
+		Frame f2 = new Frame(1,4);
+		Frame f3 = new Frame(1,4);
+		Frame f4 = new Frame(1,4);
+		Frame f5 = new Frame(1,4);
+		Frame f6 = new Frame(1,4);
+		Frame f7 = new Frame(1,4);
+		Frame f8 = new Frame(1,4);
+		Frame f9 = new Frame(1,4);
+		Frame f10 = new Frame(10,10);
+		
+		game.addFrame(f1);
+		game.addFrame(f2);
+		game.addFrame(f3);
+		game.addFrame(f4);
+		game.addFrame(f5);
+		game.addFrame(f6);
+		game.addFrame(f7);
+		game.addFrame(f8);
+		game.addFrame(f9);
+		game.addFrame(f10);
+		game.setBonus(f10, 10);	
+		
+		assertEquals(30, f10.scoreWithBonus());
+	}
 
 	
 	
